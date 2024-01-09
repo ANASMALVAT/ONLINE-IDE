f3d8e3b7a9b72781878811098ea87445f9dc5891
ANASMALVAT <an890779@dal.ca>
2024-01-09 04:21:43 -0400

    deploy

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,9 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { defineTheme } from "../lib/defineTheme";
 import useKeyPress from "../hooks/useKeyPress";
-import Footer from "./Footer";
 import OutputWindow from "./OutputWindow";
-import CustomInput from "./CustomInput";
 import OutputDetails from "./OutputDetails";
 import ThemeDropdown from "./ThemeDropdown";
 import LanguagesDropdown from "./LanguagesDropdown";
@@ -82,7 +80,7 @@ const Landing = () => {
   };
 
   const handleCompile = () => {
-    
+
     setProcessing(true);
     const formData = {
       language_id: language.id,
--- src/data/teamMembers.jsx
+++
@@ -1,8 +0,0 @@
-export const teamMembers = [
-    {
-        name:"Anas Malvat",
-        position: "Founder",
-        description: `Meet Anas, a proactive professional with a passion for coding. Despite lacking extravagant titles such as Ex Maang Software Engineer, he went above and beyond, solving 1400 coding problems and establishing a society at Dalhousie University to assist students with coding interviews. In the wake of the pandemic, Anas dedicated 20,000 hours to mastering competitive coding. Fueled by his undying passion, he founded Alpha Algo, our innovative coding platform, to share his expertise with the world.`,
-        img: 'https://alpha-images.s3.amazonaws.com/alpha-team-focused.jpg',
-    },
-]
